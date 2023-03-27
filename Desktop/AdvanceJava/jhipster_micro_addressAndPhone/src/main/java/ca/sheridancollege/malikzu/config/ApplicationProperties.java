6af2f80d78625af319024ff01b1937fb21de3d7a
z2701 <66741508+z2701@users.noreply.github.com>
2023-03-27 12:30:24 -0400

    Initial version of jhipster_micro_addressAndPhone generated by generator-jhipster@7.9.3

---
+++ Desktop/AdvanceJava/jhipster_micro_addressAndPhone/src/main/java/ca/sheridancollege/malikzu/config/ApplicationProperties.java
@@ -0,0 +1,16 @@
+package ca.sheridancollege.malikzu.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+/**
+ * Properties specific to Jhipster Micro Address And Phone.
+ * <p>
+ * Properties are configured in the {@code application.yml} file.
+ * See {@link tech.jhipster.config.JHipsterProperties} for a good example.
+ */
+@ConfigurationProperties(prefix = "application", ignoreUnknownFields = false)
+public class ApplicationProperties {
+    // jhipster-needle-application-properties-property
+    // jhipster-needle-application-properties-property-getter
+    // jhipster-needle-application-properties-property-class
+}
---
+++ Desktop/AdvanceJava/jhipster_micro_addressAndPhone/src/main/java/ca/sheridancollege/malikzu/web/rest/vm/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * View Models used by Spring MVC REST controllers.
+ */
+package ca.sheridancollege.malikzu.web.rest.vm;
